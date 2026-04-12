46b719bd081987568a35c5cc52bb09420dbdecce
babypussct <oneloveonepeopleforever@gmail.com>
2026-04-13 00:18:50 +0700

    feat: add Telegram callback query handler for payment confirmation and refactor Firebase import

--- test-run.ts
+++ test-run.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import admin from 'firebase-admin';
+import * as admin from 'firebase-admin';
 
 // Khởi tạo Firebase Admin an toàn
 if (!admin.apps.length) {
